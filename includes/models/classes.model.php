942517e9fc1f56718cc53762049723c54e63f9ed
Micael, Borgefeldt <info@mibosoft.se>
2021-08-17 14:37:03 +0200

    Removed old xml read

--- includes/models/classes.model.php
+++ includes/models/classes.model.php
@@ -8,12 +8,6 @@ class Classes {
 		$content = getContent($restCmd,$url);
 		
 		return json_decode($content, false);
-		
-/*
-		$groups = simplexml_load_file ( $baseUrl . $folder . '/grupp.xml', "SimpleXMLElement", LIBXML_NOWARNING | LIBXML_NOERROR );
-		
-		return $groups->xpath ( 'tpdb_grupp[grp_typ="T"]' );
-*/	
 	}
 }
 ?>
