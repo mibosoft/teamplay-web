ce260aa15b42e51156a9b87e9b3082c8e38d1c76
Micael, Borgefeldt <info@mibosoft.se>
2021-09-23 13:16:08 +0200

    Updated local test URL

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 /*=========== Database Configuration ==========*/
 
-//$baseUrl = 'http://localhost/tpweb/eclipse/';
+//$baseUrl = 'http://localhost/tpweb/serverside/';
 $baseUrl = 'http://teamplaycup.se/';
 //$baseUrl = 'http://89.221.249.123/';
 //$baseUrl = '../';
