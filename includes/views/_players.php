6da402df11db02e23966f56ece5459e95156fe77
Micael, Borgefeldt <info@mibosoft.se>
2024-01-12 08:46:01 +0100

    Suppressed born date

--- includes/views/_players.php
+++ includes/views/_players.php
@@ -3,7 +3,7 @@
             <td><?php echo $k->spe_nr ?></td>
             <?php echo $settings[0]->bool2 == "true" ? "-->" : "" ?>
             <td><?php echo $k->medlem ?></td>
-            <td><?php echo substr($k->personnr, 0, 4) ?></td>
+<!--            <td><?php echo substr($k->personnr, 0, 4) ?></td>  -->
             <?php echo $settings[0]->bool2 == "true" ? "<!--" : "" ?>
             <td><?php echo $k->position ?></td>
             <?php echo $settings[0]->bool2 == "true" ? "-->" : "" ?>
